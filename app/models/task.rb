beb52150dcdb4193b4faf2e7206b5fe47ddf5ff0
Andrii <beeengine@gmail.com>
2016-08-13 17:29:04 +0300

    Added deadline functionality + some design

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,18 @@
 class Task < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   belongs_to :project
+
+  def get_deadline
+    unless done
+      days = (deadline - DateTime.now.to_date).to_i
+      puts days
+      puts deadline.class
+      puts DateTime.now.to_date.class
+      if days > 0
+         return distance_of_time_in_words( DateTime.now.to_date, deadline) + ' remain'
+      else
+        return 'deadline is over!'
+      end
+    end
+  end
 end
